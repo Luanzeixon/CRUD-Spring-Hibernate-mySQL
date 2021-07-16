0d77bae6955582ccf0d46ecbf25556a68b3e48c7
Luanzeixon <luananjos765@gmail.com>
2021-07-16 16:06:31 -0300

    04_Upload_Arquivos
    Upload de fotos no cadastro

--- 02_CRUD/src/main/java/br/edu/ifrn/crud/controller/cadastroUsuarioController.java
+++ 02_CRUD/src/main/java/br/edu/ifrn/crud/controller/cadastroUsuarioController.java
@@ -1,5 +1,6 @@
 package br.edu.ifrn.crud.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,75 +20,103 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.ifrn.crud.dominio.Arquivo;
 import br.edu.ifrn.crud.dominio.Profissao;
 import br.edu.ifrn.crud.dominio.Usuario;
+import br.edu.ifrn.crud.repository.ArquivoRepository;
 import br.edu.ifrn.crud.repository.ProfissaoRepository;
 import br.edu.ifrn.crud.repository.UsuarioRepository;
 import br.ifrn.edu.crud.dto.AutoCompleteDTO;
 
-
 @Controller
 @RequestMapping("/usuarios")
 public class cadastroUsuarioController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private ProfissaoRepository profissaoRepository;
-	
+
+	@Autowired
+	private ArquivoRepository arquivoRepository;
+
 	@GetMapping("/cadastro")
 	public String entrarCadastro(ModelMap model) {
-		model.addAttribute("usuario", new Usuario()); //envia uma variavel usuario que corresponde a um novo usuario em branco que sera defenida na pagina html.
+		model.addAttribute("usuario", new Usuario()); // envia uma variavel usuario que corresponde a um novo usuario em
+														// branco que sera defenida na pagina html.
 		return "usuario/cadastro";
 	}
-	
+
 	@PostMapping("/salvar")
-	@Transactional(readOnly = false) //POIS VAI ACESSAR O BD
-	public String salvar(Usuario usuario,  RedirectAttributes attr, 
-				HttpSession sesao) {
-			
-		//Ja serve para cadastro e edicao
-		usuarioRepository.save(usuario);
-		
-		attr.addFlashAttribute("msgSucesso","Operação realizada com sucesso");
-		
-		return "redirect:/usuarios/cadastro"; //usar redirect na url usuarios, para "apagar"(atualizar) os contatos salvos do form (tem que colocar /)
+	@Transactional(readOnly = false) // POIS VAI ACESSAR O BD
+	public String salvar(Usuario usuario, RedirectAttributes attr, HttpSession sesao,
+			@RequestParam("file") MultipartFile arquivo) {
+
+		try {
+
+			if (arquivo != null && !arquivo.isEmpty()) {
+				// Normalizando nome do arquivo
+				String nomeArquivo = StringUtils.cleanPath(arquivo.getOriginalFilename());
+				Arquivo arquivoBD = new Arquivo(null, nomeArquivo, arquivo.getContentType(), arquivo.getBytes());
+
+				// Salvando a foto no banco de dados
+				arquivoRepository.save(arquivoBD);
+
+				// SE tiver uma foto deletar a antiga
+				if (usuario.getFoto() != null && usuario.getFoto().getId() != null && usuario.getFoto().getId() > 0) {
+
+					arquivoRepository.delete(usuario.getFoto());
+				}
+
+				usuario.setFoto(arquivoBD);
+			} else {
+				// se nao tiver anexado(mandando) nenhum arquivo no cadastro
+				usuario.setFoto(null);
+			}
+
+			// Ja serve para cadastro e edicao
+			usuarioRepository.save(usuario);
+			attr.addFlashAttribute("msgSucesso", "Operação realizada com sucesso");
+
+		} catch (IOException e) {
+			// Exibir erro
+			e.printStackTrace();
+		}
+
+		return "redirect:/usuarios/cadastro"; // usar redirect na url usuarios, para "apagar"(atualizar) os contatos
+												// salvos do form (tem que colocar /)
 	}
-	
-	@GetMapping("/editar/{id}") //na url vai ta o parametro que deseja editar informado no html, o path-variable pega essa variavel de mesmo nome 
-	public String iniciarEdicao(
-			@PathVariable("id") Integer idUsuario, ModelMap model,
-			HttpSession sessao
-			) {
-		
-		Usuario u = usuarioRepository.findById(idUsuario).get(); // o get é pra retornar esse usuario	
-		
-		model.addAttribute("usuario", u);  // enviar o usuario para pagina html que no form de cadastro vai ser setado esse u como usuario
-		
-		
+
+	@GetMapping("/editar/{id}") // na url vai ta o parametro que deseja editar informado no html, o
+								// path-variable pega essa variavel de mesmo nome
+	public String iniciarEdicao(@PathVariable("id") Integer idUsuario, ModelMap model, HttpSession sessao) {
+
+		Usuario u = usuarioRepository.findById(idUsuario).get(); // o get é pra retornar esse usuario
+
+		model.addAttribute("usuario", u); // enviar o usuario para pagina html que no form de cadastro vai ser setado
+											// esse u como usuario
+
 		return "/usuario/cadastro";
 	}
-	
-	//METODO QUE RECEBE O TEXTO DO AUTOCOMPLETE
+
+	// METODO QUE RECEBE O TEXTO DO AUTOCOMPLETE
 	@GetMapping("/autocompleteProfissoes")
-	@Transactional(readOnly = true) //POIS VAI ACESSAR O BD, TRUE POIS NAO TERA ALTERAÇÃO NO BD
-	@ResponseBody 					//PARA ENVIAR AS PROFISSOES EM UM FORMATO QUE O JQUERY UI ENTENTA
-	public List<AutoCompleteDTO> autocompleteProfissoes(
-			@RequestParam("term") String termo){
-		
+	@Transactional(readOnly = true) // POIS VAI ACESSAR O BD, TRUE POIS NAO TERA ALTERAÇÃO NO BD
+	@ResponseBody // PARA ENVIAR AS PROFISSOES EM UM FORMATO QUE O JQUERY UI ENTENTA
+	public List<AutoCompleteDTO> autocompleteProfissoes(@RequestParam("term") String termo) {
+
 		List<Profissao> profissoes = profissaoRepository.findByNome(termo);
-		
-		//TEM QUE MANDAR A LISTA EM DTO PARA O JQUERY ENTENTA
-		List<AutoCompleteDTO> resultados =  new ArrayList<>();
-		
-		//PERCORRER A LISTA
-		profissoes.forEach(p -> resultados.add(
-					new AutoCompleteDTO(p.getNome(),p.getId())
-				));
+
+		// TEM QUE MANDAR A LISTA EM DTO PARA O JQUERY ENTENTA
+		List<AutoCompleteDTO> resultados = new ArrayList<>();
+
+		// PERCORRER A LISTA
+		profissoes.forEach(p -> resultados.add(new AutoCompleteDTO(p.getNome(), p.getId())));
 		return resultados;
 	}
-	
+
 }
--- 02_CRUD/src/main/java/br/edu/ifrn/crud/dominio/Usuario.java
+++ 02_CRUD/src/main/java/br/edu/ifrn/crud/dominio/Usuario.java
@@ -1,35 +1,43 @@
 package br.edu.ifrn.crud.dominio;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
-@Entity 
+@Entity
 public class Usuario {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //valor id preenchido automaticamente 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // valor id preenchido automaticamente
 	private int id;
-	
+
 	@Column(nullable = false)
 	private String nome;
-	
+
 	@Column(nullable = false)
 	private String email;
-	
+
 	@Column(nullable = false)
 	private String senha;
-	
+
 	@Column(nullable = false)
 	private String sexo;
-	
-	//Muitos usuarios podem ter uma profissao, opicional falso(obrigatorio)
-	@ManyToOne(optional = false) 
+
+	// Muitos usuarios podem ter uma profissao, opicional falso(obrigatorio)
+	@ManyToOne(optional = false)
 	private Profissao profissao;
-	
-	
+
+	// Relacionamento 1-1 (so uma pesso pode ter uma foto de perfil)
+	// CascadeREMOVE = quando remover o usuario remove a foto tbm
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private Arquivo foto;
+
 	// pra que o java saiba diferenciar um usuario do outro pelo atributo id
 	@Override
 	public int hashCode() {
@@ -38,6 +46,7 @@ public class Usuario {
 		result = prime * result + id;
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -51,41 +60,61 @@ public class Usuario {
 			return false;
 		return true;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+
 	public Profissao getProfissao() {
 		return profissao;
 	}
+
 	public void setProfissao(Profissao profissao) {
 		this.profissao = profissao;
 	}
-	
+
+	public Arquivo getFoto() {
+		return foto;
+	}
+
+	public void setFoto(Arquivo foto) {
+		this.foto = foto;
+	}
+
 }
